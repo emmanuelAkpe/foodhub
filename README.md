070a1e8b282a6343caeefe52a7653e1e250447fd
E Akpe <eakpe2020\8@gmail.com>
2022-12-29 23:12:03 +0000

    home page

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
    1. Generate component
    2. Add Html
    3. Add css
+4. List Foods
+   1. Create Food Model
+   2. Create data.ts
+      1. Add sample foods
+   3. Add images to assets
+   4. Create food service
+   5. Create Home component
+      1. Add ts
+      2. Add Html
+      3. Add css
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
